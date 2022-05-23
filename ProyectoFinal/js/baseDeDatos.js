aaa06720f537483ebc9d84804a251cc05aa28ca5
Mzrt1756 <98002264+Mzrt1756@users.noreply.github.com>
2022-05-23 00:39:42 -0300

    Primer Entrega Proyecto

---
+++ ProyectoFinal/js/baseDeDatos.js
@@ -0,0 +1,12 @@
+let baseDeDatos = [{"id":1,"nombre":"Desayuno Personalizado 1","precio":1500,"imagen":"../img/DesayunoPersonalizado1.png", "descripcion":"Incluye 2 Alfajorcitos Oreo, 1 Minitorta a elección, 1 Minipastafrola, 1 Alfajorcito de Maizena, 3 Fotos Polaroid a elección."},
+{"id":2,"nombre":"Desayuno Personalizado 2","precio":2500,"imagen":"../img/DesayunoPersonalizado2.png"},
+{"id":3,"nombre":"Desayuno 15 años","precio":2700,"imagen":"../img/Desayuno15.png"},
+{"id":4,"nombre":"Bandeja Salada","precio":1800,"imagen":"../img/RegaloSalado.png"},
+{"id":5,"nombre":"Bandeja Salada y Dulce","precio":2000,"imagen":"../img/RegaloSaladoDulce.png"},
+{"id":6,"nombre":"Torta Personalizada","precio":2000,"imagen":"../img/TortaPersonalizada.png"},
+{"id":7,"nombre":"Torta Personalizada Pokemon","precio":2000,"imagen":"../img/TortaPersonalizadaPokemon.png"},
+{"id":8,"nombre":"Torta Princesa","precio":2000,"imagen":"../img/TortaPrincesa.png"},
+{"id":9,"nombre":"Torta Harry","precio":2500,"imagen":"../img/TortaHarry.png"},
+{"id":10,"nombre":"Torta Número","precio":3000,"imagen":"../img/tortanumero.jpg"},
+{"id":11,"nombre":"Torta Letra","precio":2700,"imagen":"../img/TortaLetra.png"},
+{"id":12,"nombre":"Bombones y Mini Torta","precio":2500,"imagen":"../img/Bombones+torta.png"}];
